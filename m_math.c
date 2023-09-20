d1406b2a98383d5756bd3ae7f06c622e2e781a43
derfal <sohail.bounnite@gmail.com>
2023-09-20 22:06:57 +0100

    adding addition end

--- m_math.c
+++ m_math.c
@@ -83,3 +83,32 @@ void sub(stack_t **stk, unsigned int ln_num)
 
 	free(top1);
 }
+
+/**
+ * add - adds the top two elements of the stack
+ *
+ * @stk: pointer to the pointer to the stack
+ * @ln_num: line number in the Monty script where 'add' was called
+ */
+
+void add(stack_t **stk, unsigned int ln_num)
+{
+	stack_t *top1;
+	stack_t *top2;
+
+	if (!stk || !(*stk) || !(*stk)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", ln_num);
+		exit(EXIT_FAILURE);
+	}
+
+	top1 = *stk;
+	top2 = top1->next;
+
+	top2->n += top1->n;
+
+	*stk = top2;
+	top2->prev = NULL;
+
+	free(top1);
+}
